4cf26108fc82b2f2810b3837d0b8cfc6a4def2f3
韩旭gogogo <js_bin@163.com>
2026-09-06 22:07:48 +0800

    feature: add product price list

--- src/config/products.js
+++ src/config/products.js
@@ -1,9 +1,23 @@
 import Tmax from "./tmax";
 import TmaxXT from "./tmax-xt";
-import csv from "./csv";
+import CVS from "./csv";
+import Formula from "./formula";
+import TmaxDC from "./tmax-dc";
+import FormulaM from './formula-m'
+import LC1D from './LC1D'
+import threeTS from './three-ts'
+import threeRT20 from './three-rt20'
+import threeRT6 from './three-rt6'
 
 export default {
   Tmax,
-  TmaxXT,
-  csv
+  'Tmax XT': TmaxXT,
+  CVS,
+  Formula,
+  'Tmax DC':TmaxDC,
+  'Formula M': FormulaM,
+  LC1D,
+  '3TS': threeTS,
+  '3RT20': threeRT20,
+  '3RT6': threeRT6
 };
