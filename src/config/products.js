caee3c88eb8b8231bc96074c8cfb4e9161af7aba
韩旭gogogo <js_bin@163.com>
2026-09-08 15:19:13 +0800

    feature: product list update

--- src/config/products.js
+++ src/config/products.js
@@ -3,21 +3,41 @@ import TmaxXT from "./tmax-xt";
 import CVS from "./csv";
 import Formula from "./formula";
 import TmaxDC from "./tmax-dc";
-import FormulaM from './formula-m'
-import LC1D from './LC1D'
-import threeTS from './three-ts'
-import threeRT20 from './three-rt20'
-import threeRT6 from './three-rt6'
+import FormulaM from "./formula-m";
+import LC1D from "./LC1D";
+import threeTS from "./three-ts";
+import threeRT20 from "./three-rt20";
+import threeRT6 from "./three-rt6";
+import DZ15 from "./dz15";
+import DZ20 from "./dz20";
+import NM1 from "./nm1";
+import NXB63G from "./nxb-63g";
+import NXMS from "./nxms";
+import NXM from "./nxm";
+import CDM1 from "./cdm1";
+import CDM3S from "./cdm3s";
+import CDM3LS from "./cdm3ls";
+import CDM3E from "./cdm3e";
 
 export default {
   Tmax,
-  'Tmax XT': TmaxXT,
+  "Tmax XT": TmaxXT,
   CVS,
   Formula,
-  'Tmax DC':TmaxDC,
-  'Formula M': FormulaM,
+  "Tmax DC": TmaxDC,
+  "Formula M": FormulaM,
   LC1D,
-  '3TS': threeTS,
-  '3RT20': threeRT20,
-  '3RT6': threeRT6
+  "3TS": threeTS,
+  "3RT20": threeRT20,
+  "3RT6": threeRT6,
+  DZ15,
+  DZ20,
+  NM1,
+  "NXB-63G": NXB63G,
+  NXMS,
+  NXM,
+  CDM1,
+  CDM3S,
+  CDM3LS,
+  CDM3E
 };
