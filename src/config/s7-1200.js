1bf59c4eb0b468aa79081396071b0bb62dad3407
韩旭gogogo <js_bin@163.com>
2026-09-09 15:49:26 +0800

    feature: 新增图片展示。

---
+++ src/config/s7-1200.js
@@ -0,0 +1,442 @@
+const list = [
+  {
+    orderId: "6ES72141AG400XB0",
+    model: "6ES72141AG400XB0",
+    price: "4516.27",
+    showPrice: "4516.27",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：晶体管 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/388433/30/1966/25652/6966f84dF27aa105f/07113203205418aa.jpg.avif",
+  },
+  {
+    orderId: "6ES72411CH320XB0",
+    model: "6ES72411CH320XB0",
+    price: "1058.87",
+    showPrice: "1058.87",
+    remark:
+      "系列：S7-1200 | 产品类型：通讯模块 | 通讯类型：RS422/485 | 接口类型：DB9",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/137254/30/37009/15722/6476bd12F2a6b073c/b32f69b1b050e581.jpg.avif",
+  },
+  {
+    orderId: "6ES72231PL320XB0",
+    model: "6ES72231PL320XB0",
+    price: "3286.29",
+    showPrice: "3286.29",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：16 | 数字量输出点数：16 | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 产品类型：数字量输入/输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/387969/29/3482/20098/6966f85dF1ffcbd11/07113203200630c2.jpg.avif",
+  },
+  {
+    orderId: "6ES72231BL320XB0",
+    model: "6ES72231BL320XB0",
+    price: "3286.29",
+    showPrice: "3286.29",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：16 | 数字量输出点数：16 | 数字量输入电压：DC24V | 数字量输出类型：晶体管 | 产品类型：数字量输入/输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/472689/12/10126/113670/6a4db179F93783d01/0711320320c1ef88.jpg.avif",
+  },
+  {
+    orderId: "6ES72314HF320XB0",
+    model: "6ES72314HF320XB0",
+    price: "4365.27",
+    showPrice: "4365.27",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：8 | 模拟量输出通道数：- | 模拟量输入类型：电压,电流 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/388577/32/1253/17775/6966f85fFf9a2fffe/0711320320c72e82.jpg.avif",
+  },
+  {
+    orderId: "6ES72411CH301XB0",
+    model: "6ES72411CH301XB0",
+    price: "688.41",
+    showPrice: "688.41",
+    remark:
+      "系列：S7-1200 | 产品类型：通讯扩展板 | 通讯类型：RS485 | 接口类型：-",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/55300/40/23826/18075/6476bd5fFdcb013ae/b5ee9b2716dbb8ed.jpg.avif",
+  },
+  {
+    orderId: "6ES72314HD320XB0",
+    model: "6ES72314HD320XB0",
+    price: "2680.94",
+    showPrice: "2680.94",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：4 | 模拟量输出通道数：- | 模拟量输入类型：电压,电流 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/473421/12/10821/78681/6a4dbad9Ffb80206a/07113203205fb5d4.jpg.avif",
+  },
+  {
+    orderId: "6ES72344HE320XB0",
+    model: "6ES72344HE320XB0",
+    price: "3906.75",
+    showPrice: "3906.75",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：4 | 模拟量输出通道数：2 | 模拟量输入类型：电压,电流 | 模拟量输出类型：电压,电流 | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/383250/21/23281/17213/6966f865F233d7a67/0711320320ea4889.jpg.avif",
+  },
+  {
+    orderId: "6ES72211BH320XB0",
+    model: "6ES72211BH320XB0",
+    price: "2075.57",
+    showPrice: "2075.57",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：16 | 数字量输出点数：- | 数字量输入电压：DC24V | 数字量输出类型：- | 产品类型：输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/387376/16/6354/17218/6966f856F06a9f668/0711320320a1ff8d.jpg.avif",
+  },
+  {
+    orderId: "6ES72324HD320XB0",
+    model: "6ES72324HD320XB0",
+    price: "4516.27",
+    showPrice: "4516.27",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：- | 模拟量输出通道数：4 | 模拟量输入类型：- | 模拟量输出类型：电压,电流 | 产品类型：模拟量输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/388841/1/630/17521/6966f863F6620401d/0711320320f7a177.jpg.avif",
+  },
+  {
+    orderId: "6ES72231PH320XB0",
+    model: "6ES72231PH320XB0",
+    price: "2075.57",
+    showPrice: "2075.57",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：8 | 数字量输出点数：8 | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 产品类型：数字量输入/输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/385156/30/12269/45418/6966f85bF37b48a02/071132032064cee6.jpg.avif",
+  },
+  {
+    orderId: "6ES72151AG400XB0",
+    model: "6ES72151AG400XB0",
+    price: "6808.7",
+    showPrice: "6808.7",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：2 | 数字量输入电压：DC24V | 数字量输出类型：固态-MOSFET | 模拟量输入类型：电压 | 模拟量输出类型：电流 | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/380295/23/23571/30873/6966f852F87fef999/07113203208f74fd.jpg.avif",
+  },
+  {
+    orderId: "6ES72151BG400XB0",
+    model: "6ES72151BG400XB0",
+    price: "6808.7",
+    showPrice: "6808.7",
+    remark:
+      "系列：S7-1200 | 电源电压：AC85-265V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：2 | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：电流 | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/388528/40/2114/19824/6966f853Ff11def34/07113203207733d7.jpg.avif",
+  },
+  {
+    orderId: "6ES72221BH320XB0",
+    model: "6ES72221BH320XB0",
+    price: "2075.57",
+    showPrice: "2075.57",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：16 | 数字量输入电压：DC20.4-28.8V | 数字量输出类型：晶体管 | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/382616/27/23322/19911/6966f858F6ecc8bd8/0711320320dfee59.jpg.avif",
+  },
+  {
+    orderId: "6ES72121AE400XB0",
+    model: "6ES72121AE400XB0",
+    price: "2984.29",
+    showPrice: "2984.29",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：14 | 数字量输入点数：8 | 数字量输出点数：6 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：晶体管 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/102993/38/48880/69561/65efd16dF9f8f0955/2cc5b15f7f9d1ef2.jpg.avif",
+  },
+  {
+    orderId: "6ES72315PF320XB0",
+    model: "6ES72315PF320XB0",
+    price: "6039.98",
+    showPrice: "6039.98",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：8 | 模拟量输出通道数：- | 模拟量输入类型：热电阻 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/185276/40/33636/30566/64015954Fcbf69c9f/3faf04984a78f40f.jpg.avif",
+  },
+  {
+    orderId: "6ES72324HB320XB0",
+    model: "6ES72324HB320XB0",
+    price: "2841.53",
+    showPrice: "2841.53",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：- | 模拟量输出通道数：2 | 模拟量输入类型：- | 模拟量输出类型：电压,电流 | 产品类型：模拟量输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/384285/39/13561/18341/6966f862F064c5a76/071132032021f502.jpg.avif",
+  },
+  {
+    orderId: "6ES72141BG400XB0",
+    model: "6ES72141BG400XB0",
+    price: "4516.27",
+    showPrice: "4516.27",
+    remark:
+      "系列：S7-1200 | 电源电压：AC85-265V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/385179/27/12945/26904/6966f84eF5a587f14/07113203203a303a.jpg.avif",
+  },
+  {
+    orderId: "6ES72231BH320XB0",
+    model: "6ES72231BH320XB0",
+    price: "2075.57",
+    showPrice: "2075.57",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：8 | 数字量输出点数：8 | 数字量输入电压：DC24V | 数字量输出类型：晶体管 | 产品类型：数字量输入/输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/27775/36/20685/24201/65a0f777Fdfa0f80e/cf8bce48a69ba147.jpg.avif",
+  },
+  {
+    orderId: "6ES72221BF320XB0",
+    model: "6ES72221BF320XB0",
+    price: "1299.4",
+    showPrice: "1299.4",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：8 | 数字量输入电压：DC20.4-28.8V | 数字量输出类型：晶体管 | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/381868/31/20845/17565/6966f856F69dadd12/0711320320f5e1de.jpg.avif",
+  },
+  {
+    orderId: "6ES72411AH320XB0",
+    model: "6ES72411AH320XB0",
+    price: "1058.87",
+    showPrice: "1058.87",
+    remark:
+      "系列：S7-1200 | 产品类型：通讯模块 | 通讯类型：RS232 | 接口类型：DB9",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/239797/7/4956/26462/65efd179Ff7bd5fa1/c2d45784f4df91ed.jpg.avif",
+  },
+  {
+    orderId: "6ES72151HG400XB0",
+    model: "6ES72151HG400XB0",
+    price: "6808.7",
+    showPrice: "6808.7",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：2 | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：电流 | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/147711/23/40401/26761/65a0f76eFd385a3b3/4b1142e531e09c1d.jpg.avif",
+  },
+  {
+    orderId: "6ES72221BD300XB0",
+    model: "6ES72221BD300XB0",
+    price: "827.96",
+    showPrice: "827.96",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：4 | 数字量输入电压：DC20.4-28.8V | 数字量输出类型：晶体管 | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/126470/20/20843/53882/625f4d6cE781dfb7b/0b514e181d2d0a6b.jpg.avif",
+  },
+  {
+    orderId: "6ES72221HF320XB0",
+    model: "6ES72221HF320XB0",
+    price: "1299.4",
+    showPrice: "1299.4",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：8 | 数字量输入电压：DC5-30/AC5-250V | 数字量输出类型：继电器 | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/387378/31/6084/18488/6966f858F7888eaa0/07113203203ae890.jpg.avif",
+  },
+  {
+    orderId: "6ES72221HH320XB0",
+    model: "6ES72221HH320XB0",
+    price: "2075.57",
+    showPrice: "2075.57",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：16 | 数字量输入电压：DC5-30/AC5-250V | 数字量输出类型：继电器 | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/382952/27/20075/20120/6966f859F851ab8da/0711320320e696a7.jpg.avif",
+  },
+  {
+    orderId: "6ES72315PD320XB0",
+    model: "6ES72315PD320XB0",
+    price: "3747.53",
+    showPrice: "3747.53",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：4 | 模拟量输出通道数：- | 模拟量输入类型：热电阻 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/386650/6/7917/21268/6966f860F39240074/071132032075fabf.jpg.avif",
+  },
+  {
+    orderId: "6ES72111BE400XB0",
+    model: "6ES72111BE400XB0",
+    price: "2219.7",
+    showPrice: "2219.7",
+    remark:
+      "系列：S7-1200 | 电源电压：AC85-265V | I/O总点数：10 | 数字量输入点数：6 | 数字量输出点数：4 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/210700/20/21094/38449/625f4d60E03620097/3a9234f276209b72.jpg.avif",
+  },
+  {
+    orderId: "6ES72141HG400XB0",
+    model: "6ES72141HG400XB0",
+    price: "4516.27",
+    showPrice: "4516.27",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/388369/25/2349/70667/6966f850F273b9f35/0711320320fa34cb.jpg.avif",
+  },
+  {
+    orderId: "6ES72315QF320XB0",
+    model: "6ES72315QF320XB0",
+    price: "5408.52",
+    showPrice: "5408.52",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：8 | 模拟量输出通道数：- | 模拟量输入类型：热电偶 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/381932/28/21779/21325/6966f861F5be6ddc1/0711320320f7d7f3.jpg.avif",
+  },
+  {
+    orderId: "6ES72315ND320XB0",
+    model: "6ES72315ND320XB0",
+    price: "4365.27",
+    showPrice: "4365.27",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：4 | 模拟量输出通道数：- | 模拟量输入类型：电压,电流 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/95194/6/27121/33918/625f4d74Efeb4088e/4a48edc7d77b1c56.jpg.avif",
+  },
+  {
+    orderId: "6ES72111AE400XB0",
+    model: "6ES72111AE400XB0",
+    price: "2219.7",
+    showPrice: "2219.7",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：10 | 数字量输入点数：6 | 数字量输出点数：4 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：晶体管 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/88104/29/27856/37584/625f4d5fE72e348e9/a3f956d05637ebbf.jpg.avif",
+  },
+  {
+    orderId: "6ES72266BA320XB0",
+    model: "6ES72266BA320XB0",
+    price: "2188.39",
+    showPrice: "2188.39",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：16 | 数字量输出点数：- | 数字量输入电压：DC24V | 数字量输出类型：- | 产品类型：输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/91932/7/21553/41677/62069db5Eb1259ab1/b80afca697870899.jpg.avif",
+  },
+  {
+    orderId: "6ES72121HE400XB0",
+    model: "6ES72121HE400XB0",
+    price: "2984.29",
+    showPrice: "2984.29",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：14 | 数字量输入点数：8 | 数字量输出点数：6 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/384617/22/15239/24349/6966f849F3dc1a5f6/0711320320fed41c.jpg.avif",
+  },
+  {
+    orderId: "6ES72315QD320XB0",
+    model: "6ES72315QD320XB0",
+    price: "3747.52",
+    showPrice: "3747.52",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：4 | 模拟量输出通道数：- | 模拟量输入类型：热电偶 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/167123/10/39417/21452/65312fa6F2daf0657/dc81c14850cbff9c.jpg.avif",
+  },
+  {
+    orderId: "6ES72324HA300XB0",
+    model: "6ES72324HA300XB0",
+    price: "1154.46",
+    showPrice: "1154.46",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：- | 模拟量输出通道数：1 | 模拟量输入类型：- | 模拟量输出类型：电压,电流 | 产品类型：模拟量输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/236304/32/192/35029/65312fa7Fcc259c82/f494232054b10328.jpg.avif",
+  },
+  {
+    orderId: "6ES72314HA300XB0",
+    model: "6ES72314HA300XB0",
+    price: "764.59",
+    showPrice: "764.59",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：1 | 模拟量输出通道数：- | 模拟量输入类型：电压,电流 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/188499/32/23778/31273/625f4d71E901eefb8/5a0759a6e0059b0d.jpg.avif",
+  },
+  {
+    orderId: "6ES72266DA320XB0",
+    model: "6ES72266DA320XB0",
+    price: "2188.39",
+    showPrice: "2188.39",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：4 | 数字量输入电压：- | 数字量输出类型：- | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/103209/27/22923/35795/62069db5Eeeb13531/659154a0aa9b487a.jpg.avif",
+  },
+  {
+    orderId: "6ES72151HF400XB0",
+    model: "6ES72151HF400XB0",
+    price: "9298.31",
+    showPrice: "9298.31",
+    remark:
+      "系列：S7-1200 | 电源电压：DC24V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：2 | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：电流 | 通讯接口：Profinet",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/167530/30/25163/34504/62069daeE6c1fd8de/0aeb72049cb69c62.jpg.avif",
+  },
+  {
+    orderId: "6ES72121BE400XB0",
+    model: "6ES72121BE400XB0",
+    price: "2984.29",
+    showPrice: "2984.29",
+    remark:
+      "系列：S7-1200 | 电源电压：AC85-265V | I/O总点数：14 | 数字量输入点数：8 | 数字量输出点数：6 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/381895/40/22559/23583/6966f848F31846464/07113203208da0e6.jpg.avif",
+  },
+  {
+    orderId: "6ES72171AG400XB0",
+    model: "6ES72171AG400XB0",
+    price: "9842.44",
+    showPrice: "9842.44",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：24 | 数字量输入点数：14 | 数字量输出点数：10 | 模拟量输入通道数：2 | 模拟量输出通道数：2 | 数字量输入电压：DC24V | 数字量输出类型：晶体管 | 模拟量输入类型：电压 | 模拟量输出类型：电流 | 通讯接口：ProfiNet",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/231343/19/12501/28991/65a0f772F2ecab4db/a087df7ac279581e.jpg.avif",
+  },
+  {
+    orderId: "6ES72266RA320XB0",
+    model: "6ES72266RA320XB0",
+    price: "3009.04",
+    showPrice: "3009.04",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：- | 数字量输出点数：2 | 数字量输入电压：- | 数字量输出类型：继电器 | 产品类型：输出模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/84428/22/20711/37595/62069db6E68dd61c6/ca06caaacf340a5e.jpg.avif",
+  },
+  {
+    orderId: "6ES72111HE400XB0",
+    model: "6ES72111HE400XB0",
+    price: "2219.7",
+    showPrice: "2219.7",
+    remark:
+      "系列：S7-1200 | 电源电压：DC22-28.8V | I/O总点数：10 | 数字量输入点数：6 | 数字量输出点数：4 | 模拟量输入通道数：2 | 模拟量输出通道数：- | 数字量输入电压：DC24V | 数字量输出类型：继电器 | 模拟量输入类型：电压 | 模拟量输出类型：- | 通讯接口：以太网",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/178567/20/23794/38769/625f4d60E4bb3f43d/0b55445545bedbad.jpg.avif",
+  },
+  {
+    orderId: "6ES72315PA300XB0",
+    model: "6ES72315PA300XB0",
+    price: "1154.46",
+    showPrice: "1154.46",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：1 | 模拟量输出通道数：- | 模拟量输入类型：热电阻 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/148709/30/25695/30344/625f4d74Edef0806a/13ab130af5f0e430.jpg.avif",
+  },
+  {
+    orderId: "6ES72211BF320XB0",
+    model: "6ES72211BF320XB0",
+    price: "1299.4",
+    showPrice: "1299.4",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：8 | 数字量输出点数：- | 数字量输入电压：DC24V | 数字量输出类型：- | 产品类型：输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/385661/19/11124/18283/6966f854F0588b59e/0711320320778f16.jpg.avif",
+  },
+  {
+    orderId: "6ES72970AX300XA0",
+    model: "6ES72970AX300XA0",
+    price: "483.82",
+    showPrice: "483.82",
+    remark: "系列：S7-1200",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/131799/22/20592/23494/625f4d89Ed44abad6/c1c807efeb3275bb.jpg.avif",
+  },
+  {
+    orderId: "6ES72784BD320XB0",
+    model: "6ES72784BD320XB0",
+    price: "2724.86",
+    showPrice: "2724.86",
+    remark:
+      "系列：S7-1200 | 电源电压：- | 产品类型：通讯模块 | 通讯类型：IO-Link | 接口类型：- | 最大传输速度：- | 执行协议：- | 接口数量：- | 尺寸：-",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/107029/22/28945/36460/625f4d7aEc2e06a49/47bc00eca0b7316c.jpg.avif",
+  },
+  {
+    orderId: "6ES72213BD300XB0",
+    model: "6ES72213BD300XB0",
+    price: "764.62",
+    showPrice: "764.62",
+    remark:
+      "系列：S7-1200 | 数字量输入点数：4 | 数字量输出点数：- | 数字量输入电压：DC24V | 数字量输出类型：- | 产品类型：输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/206442/38/21416/50098/625f4d6bE4a6b7364/f381e3f32158492c.jpg.avif",
+  },
+  {
+    orderId: "6ES72315QA300XB0",
+    model: "6ES72315QA300XB0",
+    price: "1154.46",
+    showPrice: "1154.46",
+    remark:
+      "系列：S7-1200 | 模拟量输入通道数：1 | 模拟量输出通道数：- | 模拟量输入类型：热电偶 | 模拟量输出类型：- | 产品类型：模拟量输入模块",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/4672/15/16688/29154/625f4d75E7a014246/c0a4a5929618cb15.jpg.avif",
+  },
+  {
+    orderId: "6ES72231BL321XB0",
+    model: "6ES7223-1BL32-1XB0",
+    price: "3278.05",
+    showPrice: "3278.05",
+    remark: "系列：S7-1200",
+    url: "https://img20.360buyimg.com/vc/s280x280_jfs/t1/456620/24/6697/37194/6a2fb104F87f525e2/0711320320ff274d.jpg.avif",
+  },
+];
+export default list;
